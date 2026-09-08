8b5a8df8b712814d6e2ce7f1501565eefcfa0a19
Willian Santos <wjsarq@gmail.com>
2026-09-08 19:13:54 -0300

    improve(data:test) update RoomCatalogRepositoryTest tests

--- data/src/commonTest/kotlin/com/bitlabbr/minhadespensa/data/RoomCatalogRepositoryTest.kt
+++ data/src/commonTest/kotlin/com/bitlabbr/minhadespensa/data/RoomCatalogRepositoryTest.kt
@@ -799,7 +799,6 @@ class RoomCatalogRepositoryTest : BaseTest() {
         val invalidEans = listOf(
             "1234567",             // 7 dígitos (< 8)
             "123456789",           // 9 dígitos (entre 8 e 13)
-            "123456789012",        // 12 dígitos
             "123456789012345"      // 15 dígitos (> 14)
         )
 
